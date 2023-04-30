795ef83639e10578a3bba47836d0c9393d9b46d0
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-04-30 09:28:57 +0200

    App creada y convertida a standalone

---
+++ src/app/app.routing.ts
@@ -0,0 +1,35 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('./app.component').then((m) => m.AppComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
+
+// import { Routes } from '@angular/router';
+
+// export const routes: Routes = [
+//   {
+//     path: '',
+//     pathMatch: 'full',
+//     loadComponent: () =>
+//       import('./blog/menu/menu.component').then((m) => m.MenuComponent),
+//   },
+//   {
+//     path: ':id',
+//     loadComponent: () =>
+//       import('./blog/article/article.component').then(
+//         (m) => m.ArticleComponent
+//       ),
+//   },
+//   {
+//     path: '**',
+//     redirectTo: '',
+//   },
+// ];
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 
-import { AppModule } from './app/app.module';
+import { MarkdownModule } from 'ngx-markdown';
+import { AppComponent } from './app/app.component';
 
+import { routes } from './app/app.routing';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom([
+      MarkdownModule.forRoot({
+        loader: HttpClient,
+      }),
+    ]),
+  ],
+}).catch((err) => console.error(err));
