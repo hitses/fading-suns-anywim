7aaa12a5946773e798463f7a26652d9cbf6077b2
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-04-30 13:36:04 +0200

    Menú desplegable por click

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { DesplegableComponent } from './desplegable/desplegable.component';
+import { Url } from './url.interface';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, DesplegableComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  universo: Array<Url> = [
+    {
+      link: '/universo/historia',
+      nombre: 'Historia',
+    },
+    {
+      link: '/universo/sociedad',
+      nombre: 'Sociedad',
+    },
+  ];
+  personajes: Array<Url> = [
+    {
+      link: '/personajes/puntos',
+      nombre: 'Puntos',
+    },
+  ];
+}
---
+++ src/app/common/header/url.interface.ts
@@ -0,0 +1,4 @@
+export interface Url {
+  link: string;
+  nombre: string;
+}
