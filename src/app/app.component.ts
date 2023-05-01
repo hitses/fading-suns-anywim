35db722bef25ba6df66a579d17ccc482a23908cf
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-05-01 18:14:15 +0200

    Menú con scroll arreglado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from './common/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './common/header/header.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterOutlet],
+  imports: [NgIf, FooterComponent, RouterOutlet],
 })
-export class AppComponent {}
+export class AppComponent {
+  mostrarMenu: boolean = false;
+  toogleMenu() {
+    this.mostrarMenu = !this.mostrarMenu;
+  }
+
+  showUniverse: boolean = false;
+  toggleUniverse() {
+    this.showUniverse = !this.showUniverse;
+  }
+
+  showHistory: boolean = false;
+  toggleHistory() {
+    this.showHistory = !this.showHistory;
+  }
+}
