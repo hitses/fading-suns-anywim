8f44061694896b579f2e3244c045d6dde3a38424
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-05-01 18:24:17 +0200

    Mejorados elementos de historia del universo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from './common/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [NgIf, FooterComponent, RouterOutlet],
+  imports: [NgIf, NgFor, FooterComponent, RouterOutlet],
 })
 export class AppComponent {
   mostrarMenu: boolean = false;
@@ -25,4 +25,79 @@ export class AppComponent {
   toggleHistory() {
     this.showHistory = !this.showHistory;
   }
+
+  universeLinks = [
+    {
+      url: 'prologo',
+      name: 'Prólogo',
+    },
+    {
+      url: 'prehistoria',
+      name: 'Prehistoria',
+    },
+    {
+      url: 'Primera República',
+      name: 'Primera República',
+    },
+    {
+      url: 'portal-de-salto',
+      name: 'El portal de Salto',
+    },
+    {
+      url: 'la-diaspora',
+      name: 'La Diáspora',
+    },
+    {
+      url: 'el-profeta-y-su-iglesia',
+      name: 'El Profeta y su Iglesia',
+    },
+    {
+      url: 'primer-contacto-los-shantor',
+      name: 'Primer contacto: los Shantor',
+    },
+    {
+      url: 'los-vau',
+      name: 'Los Vau',
+    },
+    {
+      url: 'la-guerra-de-los-ukar',
+      name: 'La guerra de los Ukar',
+    },
+    {
+      url: 'orden-del-caos',
+      name: 'Orden del Caos',
+    },
+    {
+      url: 'la-segunda-republica',
+      name: 'La Segunda República',
+    },
+    {
+      url: 'el-despojamiento',
+      name: 'El Despojamiento',
+    },
+    {
+      url: 'la-nueva-edad-oscura',
+      name: 'La Nueva Edad Oscura',
+    },
+    {
+      url: 'las-invasiones-barbaras',
+      name: 'Las Invasiones Bárbaras',
+    },
+    {
+      url: 'vladimir-une-las-estrellas',
+      name: 'Vladimir une las estrellas',
+    },
+    {
+      url: 'la-guerra-simbionte',
+      name: 'La Guerra Simbionte',
+    },
+    {
+      url: 'las-guerras-del-emperador',
+      name: 'Las Guerras del Emperador',
+    },
+    {
+      url: 'la-consolidación',
+      name: 'La Consolidación',
+    },
+  ];
 }
